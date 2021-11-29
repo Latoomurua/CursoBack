c6486b09849821df4c7e88e2362ed15a6d3f2ad4
Lautaro M <lato.murua@hotmail.com>
2021-11-29 17:48:25 -0300

    Desafio 1

--- clases.js
+++ clases.js
@@ -2,8 +2,23 @@ class Usuarios {
     constructor (nombre, apellido, libros, mascotas){
         this.nombre = nombre
         this.apellido = apellido
-        this.libros = [libros]
-        this.mascotas = [mascotas]
+        this.libros = []
+        this.mascotas = []
     }
+    getFullName(){
+        console.log(`Nombre completo del usuario: ${this.nombre} ${this.apellido}`)
+    };
+    addMascota(name){
+        this.mascotas.push(name)
+    };
+    countMascotas(){
+        console.log(`El usuario tiene un numero de mascotas de: ${this.mascotas.length}`)
+    };
+    addBook(nombreLibro, autorLibro){
+        this.libros.push({nombreLibro, autorLibro});
+    };
+    getBookNames(){
+        const indexed = this.libros.map((libros) => libros.nombreLibro);
+        console.log(indexed);
+    };
 }
-
